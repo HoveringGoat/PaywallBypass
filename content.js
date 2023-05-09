44b74706857e4d53c36d7db2c4661452ee0cdd79
Spencer Fisher <Fisher.spencer@gmail.com>
2023-05-08 23:03:18 -0700

    misc updates

---
+++ content.js
@@ -0,0 +1,23 @@
+if ((document.documentElement.textContent || document.documentElement.innerText).indexOf('was not found on this server') >= 0)
+{
+	a = document.querySelectorAll("body > *")
+	for (let i of a)
+	{
+		i.remove()
+	}
+	
+	let tag = document.createElement("p");
+	let text = document.createTextNode("Sorry this web page isn't indexed. :/");
+	tag.appendChild(text);
+	let element = document.querySelector("body");
+	element.appendChild(tag);
+}
+else
+{
+	let a = document.querySelectorAll("[id*='google-cache-hdr'], .message-container, [class*='adComponent']")
+
+	for (let i of a)
+	{
+		i.remove()
+	}
+}
