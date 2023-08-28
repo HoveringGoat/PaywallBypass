d871548081b05aaeec36a9acafd08018469b32ba
Spencer Fisher <Fisher.spencer@gmail.com>
2023-08-28 01:00:53 -0700

    minor update

--- content.js
+++ content.js
@@ -1,23 +1,32 @@
-if ((document.documentElement.textContent || document.documentElement.innerText).indexOf('was not found on this server') >= 0)
+if (matchDomain('webcache.googleusercontent.com'))
 {
-	a = document.querySelectorAll("body > *")
-	for (let i of a)
+	if ((document.documentElement.textContent || document.documentElement.innerText).indexOf('was not found on this server') >= 0)
 	{
-		i.remove()
+		a = document.querySelectorAll("body > *")
+		for (let i of a)
+		{
+			i.remove()
+		}
+		
+		let tag = document.createElement("p");
+		let text = document.createTextNode("Sorry this web page isn't indexed. :/");
+		tag.appendChild(text);
+		let element = document.querySelector("body");
+		element.appendChild(tag);
 	}
-	
-	let tag = document.createElement("p");
-	let text = document.createTextNode("Sorry this web page isn't indexed. :/");
-	tag.appendChild(text);
-	let element = document.querySelector("body");
-	element.appendChild(tag);
-}
-else
-{
-	let a = document.querySelectorAll("[id*='google-cache-hdr']")
+	else
+	{
+		let a = document.querySelectorAll("[id*='google-cache-hdr']")
 
-	for (let i of a)
-	{
-		i.remove()
+		for (let i of a)
+		{
+			i.remove()
+		}
 	}
 }
+
+function matchDomain (domains) {
+	const hostname = window.location.hostname;
+	if (typeof domains === 'string') { domains = [domains]; }
+	return domains.some(domain => hostname === domain || hostname.endsWith('.' + domain));
+  }
